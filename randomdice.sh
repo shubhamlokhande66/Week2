3fd47222d6fc783825d537a6b3ac80abf9c82fd3
shubhamlokhande66 <73211427+shubhamlokhande66@users.noreply.github.com>
2020-10-24 07:28:43 +0530

    Update randomdice.sh

--- randomdice.sh
+++ randomdice.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-# used random function to get number between 1 to 6
 
-random=$(( $RANDOM % 6 + 1 ))
-echo "$random"
+#!/bin/bash -x
+# add Two Random Dise Number
+
+number1=$(( RANDOM % 90 + 10 )) 
+number2=$(( RANDOM % 90 + 10 ))
+
+result=$(( $number1 + $number2 ))
+
+echo "First dise number is:: $number1"
+echo "Second dise number is:: $number2"
+echo "Addition of two dise number is $result"
